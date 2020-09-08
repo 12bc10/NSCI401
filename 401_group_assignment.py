490b4e58aad348011c20bff9fc62757949bac429
12bc10 <12bc10@queensu.ca>
2020-09-08 18:02:05 -0400

    Add files via upload
    Updated for multiple groups of different sizes

--- 401_group_assignment.py
+++ 401_group_assignment.py
@@ -13,35 +13,35 @@ num_students = 50
 group_size = 3
 number_groups = 17
 student_list = np.arange(0, num_students, 1)
-student_list = np.append(student_list,100)
-combinations_object = itertools.combinations(a_list, 3)
-combinations_list = list(combinations_object)
+student_list2 = np.append(student_list,100)
 
-print("there are {} combinations".format(len(combinations_list)))
-
-
-
-def getGroups(student_list, number_groups, number_students):
+def getGroups(student_list, grouping1, grouping2,  number_students):
     proj_1 = list(grouper(3, range(num_students)))
     proj_1[-1] = [proj_1[-1][0], proj_1[-1][1], 100]
     found = 0
     
     while found == 0:
-        proj_2 = partition(student_list, number_groups)
+        proj_2 = partition(student_list2, grouping1)
+        proj_3 = partition(student_list, grouping2)
+
         score = 0
             
         for r in range (num_students):          
-            groups  = np.array([np.asarray([item for item in proj_1 if r in item]), np.asarray([item for item in proj_2 if r in item])])
-            unique_ids = len(np.unique(np.ndarray.flatten(groups)))
-            num_ids = np.shape(groups[0])[1] + np.shape(groups[1])[1]
-            if num_ids == unique_ids + 1:
+            gr1 = np.asarray([item for item in proj_1 if r in item])
+            gr2 = np.asarray([item for item in proj_2 if r in item])
+            gr3 = np.asarray([item for item in proj_3 if r in item])
+            args = [gr1, gr2, gr3]
+            groups  = np.concatenate(args, axis=1)
+            unique_ids = len(np.unique(groups))
+            num_ids = np.shape(groups)[1]
+            if num_ids == unique_ids + 2:
                 score += 0
             else:
-                score += 1
+                score += num_ids-unique_ids
         print(score)
-        if score == 0:
+        if score <=8:
             found = 1
-    return(proj_1, proj_2)
+    return(proj_1, proj_2, proj_3)
             
     
 def grouper(n, iterable, fillvalue=None):
@@ -53,4 +53,4 @@ def partition (list_in, n):
     random.shuffle(list_in)
     return [list_in[i::n] for i in range(n)]
 
-proj_1, proj_2 = getGroups(student_list, 17, 50)
+proj_1, proj_2, proj_3 = getGroups(student_list, 17, 10, 50)
